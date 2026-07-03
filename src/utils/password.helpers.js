98b6972dc78c3ce49028582a206991bf6aca82f7
carlos-cas3 <ccastillob44@gmail.com>
2026-07-03 02:43:32 -0500

    change password randombytes

--- src/utils/password.helpers.js
+++ src/utils/password.helpers.js
@@ -10,7 +10,7 @@ const crypto = require("crypto");
  */
 function generateTempPassword() {
     return crypto
-        .randomBytes(8)
+        .randomBytes(10)
         .toString("base64")
         .replace(/[^a-zA-Z0-9]/g, "")
         .slice(0, 10);
